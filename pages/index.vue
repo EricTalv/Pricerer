22c02c5d734195330abec8db5e0250a13bf6a2a4
EricTalv <eric.talviste@tptlive.ee>
2019-04-08 15:57:06 +0300

    Index working with transitions

--- pages/index.vue
+++ pages/index.vue
@@ -1,48 +1,56 @@
 <template>
-    <v-container fluid grid-list-md v-bind:class="{ active: isActive }">
-        <v-layout align-center justify-center>
-            <v-flex shrink>
-                <v-card flat>
-                    <v-card-title primary-title>
-                        <div class="text-xs-center">
-                            <div class="headline">
-                                <u>Welcome To Pricerer</u>
+    <v-container
+            fluid
+            grid-list-md
+            v-bind:class="{ 'fill-height': fillHeight }"
+            v-bind="CheckItemListVisibility">
+        <v-slide-y-transition>
+            <v-layout align-center justify-center>
+                <v-flex shrink>
+                    <v-card flat>
+                        <v-card-title primary-title>
+                            <div class="text-xs-center">
+                                <div class="headline">
+                                    <u>Welcome To Pricerer</u>
+                                </div>
+                                <div class="grey--text">Think More, Spend Less</div>
                             </div>
-                            <div class="grey--text">Think More, Spend Less</div>
-                        </div>
-                    </v-card-title>
-                    <br>
-                    <v-text-field
-                            style="text-align: center"
-                            outline
-                            label="Your Product"
-                            centered
-                            v-on:keyup.enter="transitionToItems"
-                    >
-                    </v-text-field>
-                </v-card>
-            </v-flex>
-        </v-layout>
-        <v-layout
-                justify-center
-                v-if="ItemListVisible"
-        >
-            <v-flex xs2>
-                <v-card>
-                    <v-card-text>one</v-card-text>
-                </v-card >
-            </v-flex>
-            <v-flex xs2>
-                <v-card>
-                    <v-card-text>two</v-card-text>
-                </v-card>
-            </v-flex>
-            <v-flex xs2>
-                <v-card>
-                    <v-card-text>three</v-card-text>
-                </v-card>
-            </v-flex>
-        </v-layout>
+                        </v-card-title>
+                        <br>
+                        <v-text-field
+                                style="text-align: center"
+                                outline
+                                label="Your Product"
+                                centered
+                                v-on:keyup.enter="transitionToItems"
+                        >
+                        </v-text-field>
+                    </v-card>
+                </v-flex>
+            </v-layout>
+        </v-slide-y-transition>
+        <v-fade-transition>
+            <v-layout
+                    justify-center
+                    v-if="ItemListVisible"
+            >
+                <v-flex xs2>
+                    <v-card>
+                        <v-card-text>one</v-card-text>
+                    </v-card >
+                </v-flex>
+                <v-flex xs2>
+                    <v-card>
+                        <v-card-text>two</v-card-text>
+                    </v-card>
+                </v-flex>
+                <v-flex xs2>
+                    <v-card>
+                        <v-card-text>three</v-card-text>
+                    </v-card>
+                </v-flex>
+            </v-layout>
+        </v-fade-transition>
     </v-container>
 </template>
 
@@ -51,7 +59,7 @@
         data() {
             return {
                 ItemListVisible: false,
-                isActive: true,
+                fillHeight: true,
             }
         },
         methods: {
@@ -67,7 +75,9 @@
         computed: {
             CheckItemListVisibility() {
                 if (!this.ItemListVisible) {
-                    alert("not visible")
+                    this.fillHeight = true
+                } else {
+                    this.fillHeight = false
                 }
             }
         }
