dc8dc753ecf73c915ad785264b314b010f9d6513
Tehnikumi Opilane <opilane@tptlive.ee>
2019-05-20 10:52:49 +0300

    reliving the vuetify experience

--- pages/index.vue
+++ pages/index.vue
@@ -1,66 +1,91 @@
 <template>
-    <v-container
-            fluid
-            grid-list-md>
-
-        <transition-group name="fade" class="wrapper" tag="div">
-            <v-text-field
-                    key="main-content"
-                    style="text-align: center; margin-top: 15vh;"
-                    outline
-                    label="Your Product"
-                    centered
-                    v-on:keyup.enter="ItemListVisible = !ItemListVisible"
-            >
-            </v-text-field>
+    <v-app>
+        <v-container
+                fluid
+                grid-list-md>
 
 
-            <div v-if="ItemListVisible" key="dynamic" style="border: 2px solid black">
-                <v-layout justify-center>
-                    <v-flex xs2>
-                        <v-card>
-                            <v-card-text>one</v-card-text>
-                        </v-card>
-                    </v-flex>
-                    <v-flex xs2>
-                        <v-card>
-                            <v-card-text>two</v-card-text>
-                        </v-card>
-                    </v-flex>
-                    <v-flex xs2>
-                        <v-card>
-                            <v-card-text>three</v-card-text>
-                        </v-card>
-                    </v-flex>
-                </v-layout>
-            </div>
-
-        </transition-group>
+            <v-layout align-center justify-center>
 
 
-        <v-layout justify-center>
-            <v-flex shrink>
-                <v-card flat>
-                    <v-card-title primary-title>
-                        <div class="text-xs-center">
-                            <div class="headline">
-                                <u>Welcome To Pricerer</u>
-                            </div>
-                            <div class="grey--text">Think More, Spend Less</div>
-                        </div>
-                    </v-card-title>
-                    <br>
+                <v-flex text-xs-center>
+
+                    <div class="headline">
+                        <u>Welcome To Pricerer</u>
+                        <v-subheader>Think</v-subheader>
+                    </div>
+
                     <v-text-field
-                            style="text-align: center"
-                            outline
-                            label="Your Product"
                             centered
+                            key="main-content"
+                            label="Your Product"
+                            outline
+                            style="text-align: center;"
                     >
                     </v-text-field>
-                </v-card>
-            </v-flex>
-        </v-layout>
-    </v-container>
+                </v-flex>
+
+
+            </v-layout>
+
+
+            <!--  <v-layout justify-center>
+                  <v-flex shrink>
+
+                          <v-card-title primary-title>
+                              <div class="text-xs-center">
+                                  <div class="headline">
+                                      <u>Welcome To Pricerer</u>
+                                  </div>
+                                  <div class="grey&#45;&#45;text">Think More, Spend Less</div>
+                              </div>
+                          </v-card-title>
+
+                          <transition-group class="wrapper" name="fade" tag="div">
+
+                              <v-text-field
+                                      centered
+                                      key="main-content"
+                                      label="Your Product"
+                                      outline
+                                      style="text-align: center;"
+                                      v-on:keyup.enter="ItemListVisible = !ItemListVisible"
+                              >
+                              </v-text-field>
+
+
+                              <div key="dynamic" style="border: 2px solid rgba(0,0,0,0.54); border-radius: 4px; " v-if="ItemListVisible">
+
+                                  <v-container grid-list-sm>
+                                      <v-layout xs12 sm12 md12 lg12 xl12 justify-center row wrap>
+                                          <v-flex>
+                                              <v-card>
+                                                  <v-card-text>
+                                                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aut distinctio, eligendi fuga in incidunt laborum magnam nostrum possimus voluptates!
+                                                  </v-card-text>
+                                              </v-card>
+                                          </v-flex>
+                                          <v-flex>
+                                              <v-card>
+                                                  <v- card-text>two</v-card-text>
+                                              </v-card>
+                                          </v-flex>
+                                          <v-flex>
+                                              <v-card>
+                                                  <v-card-text>three</v-card-text>
+                                              </v-card>
+                                          </v-flex>
+                                      </v-layout>
+                                  </v-container>
+
+                              </div>
+
+                          </transition-group>
+
+                  </v-flex>
+              </v-layout>-->
+        </v-container>
+    </v-app>
 </template>
 
 <script>
